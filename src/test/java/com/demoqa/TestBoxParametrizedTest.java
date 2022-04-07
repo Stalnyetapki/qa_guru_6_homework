5420b0c85ea149e0b23fcd31466e934a2a1d21fb
Olga <lllallooo591@gmail.com>
2022-04-07 17:26:25 +0300

    updated homework

--- src/test/java/com/demoqa/TestBoxParametrizedTest.java
+++ src/test/java/com/demoqa/TestBoxParametrizedTest.java
@@ -1,10 +1,8 @@
 package com.demoqa;
 
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -13,6 +11,11 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 
 public class TestBoxParametrizedTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+    }
 
     @BeforeEach
     void precondition() {
@@ -25,7 +28,7 @@ public class TestBoxParametrizedTest {
     }
 
     @ValueSource(strings = {"Name1", "Name2"})
-    @ParameterizedTest(name = "Проверка формы demoqa.com \"{0}\"")
+    @ParameterizedTest(name = "Проверка формы demoqa.com по имени \"{0}\"")
     void userNameTest(String userName) {
         $("#userName").setValue(userName);
         $("#submit").click();
@@ -34,10 +37,10 @@ public class TestBoxParametrizedTest {
 
     @CsvSource({
             "Name1, name1@test.com, street 1",
-            "Bob, bob@email.com, street 2"
+            "Name2, name2@email.com, street 2"
     })
     @ParameterizedTest(name = "Проверка формы demoqa.com \"{0}\"")
-    void allInputTest(String userName, String userEmail, String userAddress) {
+    void commonFormTest(String userName, String userEmail, String userAddress) {
         $("#userName").setValue(userName);
         $("#userEmail").setValue(userEmail);
         $("#currentAddress").setValue(userAddress);
