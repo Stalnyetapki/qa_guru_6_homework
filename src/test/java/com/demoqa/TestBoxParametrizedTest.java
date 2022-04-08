d76d50520b92b463b599329fe9981ac2d6a9432b
Olga <lllallooo591@gmail.com>
2022-04-08 15:27:28 +0300

    add test by @MethodSource annotation

--- src/test/java/com/demoqa/TestBoxParametrizedTest.java
+++ src/test/java/com/demoqa/TestBoxParametrizedTest.java
@@ -4,8 +4,12 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.stream.Stream;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
@@ -41,6 +45,23 @@ public class TestBoxParametrizedTest {
     })
     @ParameterizedTest(name = "Проверка формы demoqa.com \"{0}\"")
     void commonFormTest(String userName, String userEmail, String userAddress) {
+        $("#userName").setValue(userName);
+        $("#userEmail").setValue(userEmail);
+        $("#currentAddress").setValue(userAddress);
+        $("#submit").click();
+        $("#output").shouldHave(text(userName), text(userEmail), text(userAddress));
+    }
+
+    static Stream<Arguments> mixedArgumentsTestDataProvider() {
+        return Stream.of(
+                Arguments.of("Name1", "name1@test.com", "street 1"),
+                Arguments.of("Name2", "name2@test.com", "street 2")
+        );
+    }
+
+    @MethodSource(value = "mixedArgumentsTestDataProvider")
+    @ParameterizedTest(name = "Проверка формы demoqa.com c помощью @MethodSource \"{0}\"")
+    void mixedArgumentsTest(String userName, String userEmail, String userAddress){
         $("#userName").setValue(userName);
         $("#userEmail").setValue(userEmail);
         $("#currentAddress").setValue(userAddress);
